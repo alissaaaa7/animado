dcfd318a4b8ba2bca6e71e62738caa8fd9807445
lis <73298516+alissaaaa7@users.noreply.github.com>
2025-06-22 14:08:23 -0300

    script.js

---
+++ script.js
@@ -0,0 +1,31 @@
+let list = document.querySelectorAll('.item')
+
+let next = document.getElementById('next')
+
+let prev = document.getElementById('prev')
+
+let count = list.length //3
+
+// 0, 1, 2
+
+// count = 3
+
+let active = 0
+
+next.onclick = () => {
+    let active0ld = document.querySelector ('.active')
+    active0ld.classList.remove('active')
+
+    active = active >= count -1 ? 0 : active + 1
+
+    list[active].classList.add('active')
+}
+
+prev.onclick = () => {
+    let active0ld = document.querySelector ('.active')
+    active0ld.classList.remove('active')
+
+    active = active <= 0 ? count -1 : active - 1
+
+    list[active].classList.add('active')
+}
